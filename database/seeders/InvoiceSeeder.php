a993e921b29b8a7bf949373ee2e28e7d6c6be246
Ra-is <osumanurais@gmail.com>
2023-07-01 12:45:56 +0000

    added seeders

---
+++ database/seeders/InvoiceSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Invoice;
+use App\Models\Item;
+use Illuminate\Database\Seeder;
+
+class InvoiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $invoice = Invoice::create();
+        $items = Item::factory(10)->create(['invoice_id' => $invoice->id]);
+    }
+}
