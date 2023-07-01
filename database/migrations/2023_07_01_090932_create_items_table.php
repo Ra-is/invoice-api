804d37b52eb3efa2f75fd24adb964ec775cd235f
Ra-is <osumanurais@gmail.com>
2023-07-01 09:35:09 +0000

    added items model and migration

---
+++ database/migrations/2023_07_01_090932_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id');
+            $table->decimal('unit_price', 8, 2);
+            $table->integer('quantity');
+            $table->decimal('amount', 8, 2);
+            $table->string('description');
+            $table->tinyInteger('available_quantity')->default(0);
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
