b6522632d190a2a2c9bc055757713a2d79494aba
Ra-is <osumanurais@gmail.com>
2023-07-01 12:19:59 +0000

    modified

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerInvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('invoices')->group(function () {
+    Route::get('/', [CustomerInvoiceController::class,'index']);
+    Route::post('/', [CustomerInvoiceController::class,'create']);
+    Route::get('/{id}', [CustomerInvoiceController::class,'get']);
+    Route::put('/{id}', [CustomerInvoiceController::class,'update']);
+    Route::delete('/{id}', [CustomerInvoiceController::class,'delete']);
 });
