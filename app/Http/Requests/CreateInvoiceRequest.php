4ea46d1d28b9bc395177065c2eada788a3e08c43
Ra-is <osumanurais@gmail.com>
2023-07-01 11:40:45 +0000

    modified create invoice request

--- app/Http/Requests/CreateInvoiceRequest.php
+++ app/Http/Requests/CreateInvoiceRequest.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
 
 class CreateInvoiceRequest extends FormRequest
 {
@@ -13,7 +18,7 @@ class CreateInvoiceRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +29,36 @@ class CreateInvoiceRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'issue_date' => 'required|date',
+            'due_date' => 'required|date',
+            'customer_id' => 'required|exists:customers,id',
+            'items' => 'required|array',
+            'items.*.unit_price' => 'required|numeric',
+            'items.*.quantity' => 'required|integer',
+            'items.*.amount' => 'required|numeric',
+            'items.*.description' => 'required|string',
         ];
+    }
+
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(
+                [
+                 'errors'        => $errors
+                ],
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 }
